8c35d254a8684f83277dfe8ea119df8c0ba0905a
hengchu <hengchu.zhang@gmail.com>
2014-10-17 00:24:09 -0400

    header table view now displays no file message when no file is open

--- MDS9/View Controllers/MDSHeaderTableViewController.m
+++ MDS9/View Controllers/MDSHeaderTableViewController.m
@@ -54,22 +54,35 @@
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
 {
-  return self.headers.count;
+  return (self.headers.count) ? self.headers.count : 1;
 }
 
 - (CGFloat)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row
 {
-  RACTupleUnpack(NSString *key, NSString *value, NSString *comment) = self.headers[row];
-  
-  MDSHeaderTableRowView *view = [[MDSHeaderTableRowView alloc] initWithFrame:NSMakeRect(0, 0, 1, 1)];
-  
-  view.key     = key;
-  view.value   = value;
-  view.comment = comment;
-  
-  NSSize size  = view.intrinsicContentSize;
-  
-  return size.height;
+  if (self.headers.count) {
+    
+    RACTupleUnpack(NSString *key, NSString *value, NSString *comment) = self.headers[row];
+    
+    MDSHeaderTableRowView *view = [[MDSHeaderTableRowView alloc] initWithFrame:NSMakeRect(0, 0, 1, 1)];
+    
+    view.key     = key;
+    view.value   = value;
+    view.comment = comment;
+    
+    NSSize size  = view.intrinsicContentSize;
+    
+    return size.height;
+    
+  } else {
+    
+    NSTextField *textLabel = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 1, 1)];
+    textLabel.stringValue = @"No Open File";
+    textLabel.font = [NSFont systemFontOfSize:22.0];
+    textLabel.alignment = NSCenterTextAlignment;
+    [textLabel sizeToFit];
+    return textLabel.frame.size.height;
+    
+  }
 }
 
 #pragma mark - Table view delegate
@@ -78,21 +91,33 @@
    viewForTableColumn:(NSTableColumn *)tableColumn
                   row:(NSInteger)row
 {
-  
-  RACTupleUnpack(NSString *key, NSString *value, NSString *comment) = self.headers[row];
-  
-  MDSHeaderTableRowView *view = [[MDSHeaderTableRowView alloc] initWithFrame:NSMakeRect(0, 0, 1, 1)];
-  
-  view.key                   = key;
-  view.value                 = value;
-  view.comment               = comment;
-  view.wantsLayer            = YES;
-  view.layer.backgroundColor = [NSColor clearColor].CGColor;
-  
-  NSSize size = view.intrinsicContentSize;
-  view.frame = NSMakeRect(0, 0, size.width, size.height);
-  
-  return view;
+  if (self.headers.count) {
+    RACTupleUnpack(NSString *key, NSString *value, NSString *comment) = self.headers[row];
+    
+    MDSHeaderTableRowView *view = [[MDSHeaderTableRowView alloc] initWithFrame:NSMakeRect(0, 0, 1, 1)];
+    
+    view.key                   = key;
+    view.value                 = value;
+    view.comment               = comment;
+    view.wantsLayer            = YES;
+    view.layer.backgroundColor = [NSColor clearColor].CGColor;
+    
+    NSSize size = view.intrinsicContentSize;
+    view.frame = NSMakeRect(0, 0, size.width, size.height);
+    
+    return view;
+  } else {
+    NSTextField *textLabel    = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 1, 1)];
+    textLabel.stringValue     = @"No Open File";
+    textLabel.font            = [NSFont fontWithName:@"HelveticaNeue-Light" size:22.0];
+    textLabel.alignment       = NSCenterTextAlignment;
+    textLabel.bordered        = NO;
+    textLabel.editable        = NO;
+    textLabel.selectable      = NO;
+    textLabel.textColor       = [NSColor lightGrayColor];
+    textLabel.backgroundColor = [NSColor clearColor];
+    return textLabel;
+  }
 }
 
 
